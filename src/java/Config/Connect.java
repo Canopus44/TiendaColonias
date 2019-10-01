e79f1a8e5d01f24f5435da24a1260cdce17a5968
canopus44 <canopus44@gmail.com>
2019-10-01 15:46:51 -0500

    Update Connect.java

--- src/java/Config/Connect.java
+++ src/java/Config/Connect.java
@@ -2,6 +2,7 @@ package Config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -9,14 +10,18 @@ import java.sql.DriverManager;
  */
 public class Connect {
     Connection Conn;
+<<<<<<< Updated upstream
     String url = "jdbc:mariadb://localhost:3306/bd_tiendaColinias?";
+=======
+    String url = "com.mysql.jdbc.Driver";
+>>>>>>> Stashed changes
     String user = "root";
     String password = "";
     public Connection ConnectDataBase(){
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             Conn = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {            
         }
         return Conn;
     }
